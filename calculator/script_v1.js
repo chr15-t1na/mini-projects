718f4c5728e80ef4c36609be00a67d1d84a8bca3
christina-wiederkehr <84911903+christina-wiederkehr@users.noreply.github.com>
2021-07-05 22:41:06 +1000

    Update script_v1.js

--- calculator/script_v1.js
+++ calculator/script_v1.js
@@ -9,11 +9,12 @@ const NUMBER_BUTTONS = { "one" : 1,
 "eight" : 8,
 "nine" : 9,
 "zero" : 0,
+"dot" : "."}
+const OPERATOR_BUTTONS = { 
 "sum" : "+",
 "sub" : "-",
 "multi" : "*",
-"divi" : "/",
-"dot" : "."
+"divi" : "/"
 };
 
 
@@ -22,65 +23,61 @@ const NUMBER_BUTTONS = { "one" : 1,
 
 var btns = document.querySelectorAll('button'); //get all buttons
     btns.forEach( btn => {
-        btn.addEventListener('click', f = (e) => disp.numbers(e.currentTarget.id));
+        btn.addEventListener('click', f = (e) => {disp.numbers(e.currentTarget.id); disp.others_bts(e.currentTarget.id)});
     });
 
  
 
 var disp = (function display () {
-    var numberStorage1 = "";
-    var numberStorage2 = "";
+    var numberStorage = [];
+    var operatorStorage = [];
+    var numbs = "";
+
     function addNumb(val){
-        if(/[+\-\*\/]/.test(numberStorage1) == false){    //safes numbers in other storage after operation sign
-            numberStorage1 += String(val); 
-            SCREEN.innerText = numberStorage1;
-        } else {
-            numberStorage2 += String(val); 
-            SCREEN.innerText = numberStorage2;
-        }
+            numberStorage.push(val);
+            SCREEN.innerText = numberStorage[-1];
+    }
+    function addOperators(val){
+            operatorStorage += String(val); 
+            SCREEN.innerText = operatorStorage;
     }   
     return {
         numbers : function (id) {
                 if (id in NUMBER_BUTTONS == true) {
-                    var number1 = NUMBER_BUTTONS[id];
-                    return addNumb(number1);            //passing on clicked button to storage function
-                } else {switch (id) {
+                    numbs += String(NUMBER_BUTTONS[id]); 
+                    SCREEN.innerText = numbs;
+                } else { 
+                    addNumb(numbs);
+                    numbs = "";  
+                    var oprts = OPERATOR_BUTTONS[id];
+                    return addOperators(oprts);}
+                },         
+        others_bts : function (id) {
+                        switch (id) {
                         case "enter":
-                            result = eval(numberStorage1+numberStorage2);
-                            numberStorage1 = "";
+                            result = eval(numberStorage[0]+operatorStorage[0]+numberStorage[1]);
+                            numberStorage = [];
+                            operatorStorage = [];
+                            console.log(result)
                             addNumb(result);
                             break;
                         case "dele":
-                            if (SCREEN.innerText === numberStorage1) {
-                                numberStorage1 = "";
-                                addNumb(numberStorage1);
-                            } else {
-                                numberStorage2 = "";
-                                addNumb(numberStorage2);
-                            }
-                            break;
+                            numberStorage.pop(numberStorage[-1])
                         case "clr":
-                            numberStorage1 = "";
-                            numberStorage2 = "";
+                            numberStorage = [];
+                            operatorStorage = [];
                             SCREEN.innerText = 0;
                             break;
                         case "perc":
-                            if (SCREEN.innerText === numberStorage1) {
-                                numberStorage1 = "";
-                                result = SCREEN.innerText/100;
-                                addNumb(result);
-                            } else {
-                                numberStorage2 = "";
-                                result = SCREEN.innerText/100;
-                                addNumb(result);
-                            }
+                            console.log(SCREEN.innerText)
+                            percentage = SCREEN.innerText/100;
+                            numberStorage.splice(-1, 1, percentage);
+                            SCREEN.innerText = percentage;
                             break;
                         case "swtch":
 
-                            console.log('true')
                             break;
                         }
-                    };
-                },
-            };         
+                    },
+                }         
     })();
